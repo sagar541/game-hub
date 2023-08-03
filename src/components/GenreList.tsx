1ac34468f793fb2690539fa4ea1523c972739b9f
Sagar Agrawal <agrawals541@gmail.com>
2023-08-03 14:19:15 +1000

    Fetching genre with generic hook

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,15 @@
+import useData from "../hooks/useData";
+import useGenres, { Genre } from "../hooks/useGenres";
+
+const GenreList = () => {
+  const { data, error, isLoading } = useGenres();
+  return (
+    <ul>
+      {data.map((genre) => (
+        <li key={genre.id}>{genre.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default GenreList;
